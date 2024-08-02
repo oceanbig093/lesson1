3b35315c101bb59faf3f0f0adc79f84aa98418cd
oceanbig093 <innervision41@mail.ru>
2024-08-02 12:25:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#1st program
+print((9 ** 0.5)* 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(((1234 % 1004)// 10)+((5678 % 5008)//10))
+#4th program
+print((int(((42.13 % 1)*100)) == int(13.42)) or (int((13.42 % 1) * 100)) == int(42.13))
